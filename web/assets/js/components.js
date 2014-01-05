9681c9f1cdb6217d46a65776cf3e8c0595ae44cb
Jan Machacek <jan.machacek@gmail.com>
2014-01-05 19:50:40 +0000

    chart updates

--- web/assets/js/components.js
+++ web/assets/js/components.js
@@ -1,70 +1,67 @@
-angular.module('numbers', []).
-    directive('chart', function() {
-        return {
-            scope: { data: '@' },
-            restrict: 'A',
-            link: function($scope, element, attrs) {
-                attrs.$observe('data', function(rawValue) {
-                    console.log(rawValue);
-                    /*
-                    var data = JSON.parse(rawValue);
-                    var x = d3.scale.linear()
-                            .domain([0, d3.max(data)])
-                            .range([0, 420]);
+angular.module('numbers', [])
+  .directive('chart', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+              attrs.$observe('chart', function(rawValue) {
+                var data = JSON.parse(rawValue); //[4, 8, 15, 16, 23, 42];
 
-                    d3.select(".chart")
-                      .selectAll("div")
-                        .data(data)
-                      .enter().append("div")
-                        .style("width", function(d) { return x(d) + "px"; })
-                        .text(function(d) { return d; });
-                    */
-                });
+                var x = d3.scale.linear()
+                    .domain([0, d3.max(data)])
+                    .range([0, 420]);
+
+                var p = d3.select(element[0]).selectAll("div").data(data);
+                p.enter().append("div")
+                    .style("width", function(d) { return x(d) + "px"; })
+                    .text(function(d) { return d; });
+                p.transition()
+                    .style("width", function(d) { return x(d) + "px"; })
+                    .text(function(d) { return d; });
+                p.exit().remove();
+              });
             }
-        };
-    }).
-    directive('tabs', function() {
-        return {
-            restrict: 'E',
-            transclude: true,
-            scope: {},
-            controller: function($scope, $element) {
-                var panes = $scope.panes = [];
+    };
+  })
+  .directive('tabs', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      controller: function($scope, $element) {
+        var panes = $scope.panes = [];
 
-                $scope.select = function(pane) {
-                    angular.forEach(panes, function(pane) {
-                        pane.selected = false;
-                    });
-                    pane.selected = true;
-                };
+        $scope.select = function(pane) {
+        angular.forEach(panes, function(pane) { pane.selected = false; });
+        pane.selected = true;
+      };
 
-                this.addPane = function(pane) {
-                    if (panes.length == 0) $scope.select(pane);
-                    panes.push(pane);
-                };
-            },
-            template:
-                '<div class="tabbable">' +
-                    '<ul class="nav nav-tabs">' +
-                    '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
-                    '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
-                    '</li>' +
-                    '</ul>' +
-                    '<div class="tab-content" ng-transclude></div>' +
-                    '</div>',
-            replace: true
+      this.addPane = function(pane) {
+        if (panes.length == 0) $scope.select(pane);
+        panes.push(pane);
         };
-    }).
-    directive('pane', function() {
-        return {
-            require: '^tabs',
-            restrict: 'E',
-            transclude: true,
-            scope: { title: '@' },
-            link: function(scope, element, attrs, tabsCtrl) {
-                tabsCtrl.addPane(scope);
-            },
-            template: '<div class="tab-pane" ng-class="{active: selected}" ng-transclude></div>',
-            replace: true
-        };
-    })
+      },
+      template:
+        '<div class="tabbable">' +
+        '<ul class="nav nav-tabs">' +
+        '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
+        '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
+        '</li>' +
+        '</ul>' +
+        '<div class="tab-content" ng-transclude></div>' +
+        '</div>',
+      replace: true
+    };
+  })
+  .directive('pane', function() {
+    return {
+      require: '^tabs',
+      restrict: 'E',
+      transclude: true,
+      scope: { title: '@' },
+      link: function(scope, element, attrs, tabsCtrl) {
+        tabsCtrl.addPane(scope);
+      },
+      template: '<div class="tab-pane" ng-class="{active: selected}" ng-transclude></div>',
+      replace: true
+    };
+  })
