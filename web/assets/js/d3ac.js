20f64985907e24e54c736eab4251572792742511
Jan Machacek <jan.machacek@gmail.com>
2014-01-06 11:59:30 +0000

    Tidied up the D3 code

--- web/assets/js/d3ac.js
+++ web/assets/js/d3ac.js
@@ -1,25 +1,31 @@
-angular.module('dthree', [])
-  .directive('chart', function() {
+angular.module('d3.directives', ['d3.core'])
+  .directive('chart', ['d3Service', function(d3Service) {
     return {
-      restrict: 'A',
+      restrict: 'E',
+      transclude: true,
+      scope: { data: '@' },
+      template: '<div class="chart" ng-transclude></div>',
+      replace: true,
       link: function(scope, element, attrs) {
-              attrs.$observe('chart', function(rawValue) {
-                var data = JSON.parse(rawValue);
+              d3Service.d3().then(function(d3) {
+                function fmt(element, x) {
+                  element.style("width", function(d) { return x(d) + "px"; })
+                         .text(function(d) { return d; });
+                }
 
-                var x = d3.scale.linear()
-                    .domain([0, d3.max(data)])
-                    .range([0, 420]);
+                attrs.$observe('data', function(rawValue) {
+                  var data = JSON.parse(rawValue);
 
-                var p = d3.select(element[0]).selectAll("div").data(data);
-                p.enter().append("div")
-                    .style("width", function(d) { return x(d) + "px"; })
-                    .text(function(d) { return d; });
-                p.transition()
-                    .style("width", function(d) { return x(d) + "px"; })
-                    .text(function(d) { return d; });
-                p.exit().remove();
+                  var x = d3.scale.linear()
+                      .domain([0, d3.max(data)])
+                      .range([0, 420]);
+
+                  var p = d3.select(element[0]).selectAll("div").data(data);
+                  fmt(p.enter().append("div"), x);
+                  fmt(p.transition(), x);
+                  p.exit().remove();
+                });
               });
             }
     };
-  })
-
+  }]);
